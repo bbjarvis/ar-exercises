97567151418f5f646708946fdf574aacb6b21e12
bbjarvs <brettbjarvis@gmail.com>
2022-01-09 23:04:05 +0000

    add exercise_6, creating employee records in db

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+  def create_employee (store, first_name, last_name, hourly_rate)
+    store.employees.create(
+      first_name: first_name,
+      last_name: last_name,
+      hourly_rate: hourly_rate
+    )
+  end
+create_employee(@store1, "Khurram", "Virani", 60)
+create_employee(@store1, "Brett", "Jarvis", 1000)
+create_employee(@store1, "Jess", "Gunn", 90)
+create_employee(@store1, "Emilia", "Leigh", 50)
+create_employee(@store1, "Billy", "Jerry", 40)
+create_employee(@store2, "Keith", "Richards", 400)
+create_employee(@store2, "Mick", "Jagger", 65)
